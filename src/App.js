926f0933873b4bdbf7fa7540071d1ff5719631b0
Nico Bender <nicolasbender@gmail.com>
2023-12-23 14:30:58 -0300

    Base

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import YouTube from 'react-youtube';
+import axios from 'axios';
+
+function App() {
+
+  const API_URL = `http:\\www.prueba\${algo}`;
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
